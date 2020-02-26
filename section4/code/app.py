acd934a4c254c640b8e48ee7dd61e8c0d848284f
Will Cook <hello@doubleucook.com>
2020-02-26 16:38:04 +1300

    updated gitignore, added initial jwt auth handling

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 PORT = 5000
 app = Flask(__name__)
 api = Api(app)
+app.secret_key = 'jose'
+
+jwt = JWT(app, authenticate, identity) # /auth endpoint
 
 # fake db
 items = []
@@ -15,13 +21,18 @@ class Student(Resource):
     return { 'student': name }
 
 class Item(Resource):
+  @jwt_required()
   def get(self, name):
-    for item in items:
-      if item['name'] == name:
-        return item
-    return { 'message': f"no items found with {name}" }, 404
+    #  next returns the first item found in the filter function
+    # None is the default value
+    item = next(filter(lambda x: x['name'] == name, items), None) 
+    return { 'item': item }, 200 if item else 404    
 
   def post(self, name):
+    # if name is found (not None), already exists
+    if next(filter(lambda x: x['name'] == name, items), None):
+      return { 'message': f"An item with name {name} already exists" }, 400
+    
     payload = request.get_json()
     item = { 'name': name, 'price': payload['price'] }
     items.append(item)
