2cc73246868b71b12a49bb29526056be844fa626
Will Cook <hello@doubleucook.com>
2020-02-27 17:32:32 +1300

    changed /items route to use the db file

--- section5/code/item.py
+++ section5/code/item.py
@@ -103,4 +103,16 @@ class Item(Resource):
 
 class ItemList(Resource):
   def get(self):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    
+    query = "SELECT * FROM items"
+    result = cursor.execute(query)
+    items = []
+    for row in result:
+      items.append({'name': row[0], 'price': row[1]})
+
+    connection.commit()
+    connection.close()
+
     return { 'items': items }
