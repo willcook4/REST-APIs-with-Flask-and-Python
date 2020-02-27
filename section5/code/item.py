cfca27803847d6c9dbb381b14c9680db494a3a04
Will Cook <hello@doubleucook.com>
2020-02-27 17:23:35 +1300

    updated item put method to use the db

--- section5/code/item.py
+++ section5/code/item.py
@@ -58,6 +58,17 @@ class Item(Resource):
     if row:
       return { 'item': {'name': row[0], 'price': row[1]}}
 
+  @classmethod
+  def update(cls, item):
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    query = "UPDATE items SET price=? WHERE name=?"
+    cursor.execute(query, (item['price'], item['name']))
+    connection.commit()
+    connection.close()
+
+
   @jwt_required()
   def delete(self, name):
     connection = sqlite3.connect('data.db')
@@ -74,13 +85,21 @@ class Item(Resource):
   def put(self, name):
     data = Item.parser.parse_args()
 
-    item = next(filter(lambda x: x['name'] == name, items), None)
+    item = self.find_by_name(name)
+    updated_item = { 'name': name, 'price': data['price'] }
+    
     if item is None:
-      item = { 'name': name, 'price': data['price'] }
-      items.append(item)
+      try:
+        self.insert(updated_item)
+      except:
+        return {'message': 'an error occured inserting the item'}, 500
     else:
-      item.update(data)
-    return item
+      try:
+        self.update(updated_item)
+      except:
+        return {'message': 'an error occured inserting the item'}, 500
+    
+    return updated_item
 
 class ItemList(Resource):
   def get(self):
