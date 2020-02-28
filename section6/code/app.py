a821a4d2ed261ccbc158d46e6285fab9ccfdfeb2
Will Cook <hello@doubleucook.com>
2020-02-28 17:24:01 +1300

    converted over to SQLAlchemy to create tables if needed

--- section6/code/app.py
+++ section6/code/app.py
@@ -16,13 +16,13 @@ api = Api(app)
 app.secret_key = 'jose'
 app.config['JWT_AUTH_URL_RULE'] = '/login' # change the default JWT /auth endpoint to /login 
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800) # set JWT token to expire within 30 min
-jwt = JWT(app, authenticate, identity) 
 
-# Student inherits from the Resource class
-# class Student(Resource):
-#   # get is a class method to handle the matching HTTP Verb GET
-#   def get(self, name):
-#     return { 'student': name }
+# flask decorator
+@app.before_first_request
+def create_tables():
+  db.create_all()
+
+jwt = JWT(app, authenticate, identity) 
 
 ### Endpoints
 # api.add_resource(Student, '/student/<string:name>') # eg. http://localhost:5000/student/Will
--- section6/code/create_tables.py
+++ section6/code/create_tables.py
@@ -1,19 +1,22 @@
-import sqlite3
 
-connection = sqlite3.connect('data.db')
-cursor = connection.cursor()
+## Now using sqlalchemy to create the tables
+print('This file no longer does anything, Now using sqlalchemy to create the tables')
+# import sqlite3
 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-cursor.execute(create_table)
+# connection = sqlite3.connect('data.db')
+# cursor = connection.cursor()
 
-print('Creating users table if it does not exist')
+# create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+# cursor.execute(create_table)
 
-create_items_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
-cursor.execute(create_items_table)
-print('Creating items table if it does not exist')
+# print('Creating users table if it does not exist')
 
-# cursor.execute("INSERT INTO items VALUES ('test', 10.99)")
-# print('Creating a test item')
+# create_items_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, name text, price real)"
+# cursor.execute(create_items_table)
+# print('Creating items table if it does not exist')
 
-connection.commit()
-connection.close()
+# # cursor.execute("INSERT INTO items VALUES ('test', 10.99)")
+# # print('Creating a test item')
+
+# connection.commit()
+# connection.close()
